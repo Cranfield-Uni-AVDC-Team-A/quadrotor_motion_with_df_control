86879bdd759c41329b1835a86f158a9caf8744e5
Miguel <miferco97@gmail.com>
2021-06-04 17:25:53 +0200

    bugs solved

--- subpackages/TrajectoryGenerator/package/src/main_trajectory_publisher.cpp
+++ subpackages/TrajectoryGenerator/package/src/main_trajectory_publisher.cpp
@@ -7,35 +7,9 @@
 #include "nav_msgs/Odometry.h"
 #include "sensor_msgs/Imu.h"
 
-nav_msgs::Odometry odom_msg;
-void PoseCallback(const geometry_msgs::PoseStamped& msg){
-    odom_msg.header = msg.header;
-    odom_msg.header.frame_id = "map";
-    odom_msg.pose.pose = msg.pose;
-}
-
-void TwistCallback(const geometry_msgs::TwistStamped& msg){
-    odom_msg.header = msg.header;
-    odom_msg.twist.twist.linear = msg.twist.linear;
-}
-
-void IMUCallback(const sensor_msgs::Imu& msg ){
-
-    odom_msg.twist.twist.angular = msg.angular_velocity;
-}
-
-
 int main(int argc, char **argv){
     ros::init(argc, argv, "trajectory_publisher_node");
 
-    ros::NodeHandle nh;
-    ros::Publisher odom_pub = nh.advertise<nav_msgs::Odometry>("drone/odom",1);
-    
-    ros::Subscriber pose_sub = nh.subscribe("drone1/self_localization/pose",1,PoseCallback);
-    ros::Subscriber speed_sub = nh.subscribe("drone1/self_localization/speed",1,TwistCallback);
-    ros::Subscriber imu_sub = nh.subscribe("drone1/sensor_measurement/imu",1,IMUCallback);
-
-    
     
     TrajectoryPublisher trajectory_publisher(Trajectory_type::eth_spline_non_linear);
     // TrajectoryPublisher trajectory_publisher(Trajectory_type::eth_spline_linear);
@@ -48,8 +22,6 @@ int main(int argc, char **argv){
     {
         //updating all the ros msgs
         
-        ros::spinOnce();
-        odom_pub.publish(odom_msg);
         ros::spinOnce();
         //running the localizer
         trajectory_publisher.run();
